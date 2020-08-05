51f6cb947f2bfbe2d995455538aec4583bda66d6
sruthi-shankar <66329546+sruthi-shankar@users.noreply.github.com>
2020-08-05 11:09:47 -0700

    changes made in few files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
     
     def current_user
         if session[:user_id].present?
-            @current_user ||= User.find session[:user_id] # ||= will only assign
-            # @current_user if it is nil. otherwise continue using it as it is
+            @current_user ||= User.find session[:user_id] 
+           
         end
     end
     helper_method :current_user 
